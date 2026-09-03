b90221c72b71f176758723e4934c3f785de8ff1d
sairamtejasaladi <sairamtejasaladi@gmail.com>
2026-09-03 17:00:28 +0530

    Implement Gradient descent optimizer (Math Foundations 1/6)

---
+++ foundations/gradient_descent.py
@@ -0,0 +1,14 @@
+class Solution:
+    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
+        # Objective function: f(x) = x^2
+        # Derivative:         f'(x) = 2x
+        # Update rule:        x = x - learning_rate * f'(x)
+        # Round final answer to 5 decimal places
+        fn = init**2 
+        i = 0 
+        while i < iterations : 
+            init = init-learning_rate*2*(init)
+            i +=1 
+        
+        return round(init, 5)
+        
